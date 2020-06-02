4df397a1f5e21856ecdf95512f1638239f39e607
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2020-06-02 14:05:41 -0400

    añadida la impersión por pantalla del menú principal

--- main.c
+++ main.c
@@ -5,5 +5,27 @@
 int main()
 {
 
+    int opcion;
+    printf("Bienvenido a nuestro sistema recomendador de peliculas!\n\n");
+
+    do{
+
+        printf("Que desea hacer?\n\n");
+        printf("Opcion 1: Cargar lista de peliculas\n");
+        printf("Opcion 2: Ver listas de peliculas\n");
+        printf("Opcion 3: Ver peliculas de una lista\n");
+        printf("Opcion 4: Ver informacion de una pelicula\n");
+        printf("Opcion 5: Marcar pelicula como preferida\n");
+        printf("Opcion 6: Recomendar pelicula\n");
+        printf("Opcion 0: Salir del programa\n");
+        printf("\n");
+        printf("Ingrese una opcion: ");
+
+        scanf("%d", &opcion);
+        if(opcion < 0 || opcion > 6) printf("\nDebe ingresar una opcion correcta\n\n");
+
+    }while(opcion < 0 || opcion > 6);
+
+    system("pause");
     return 0;
 }
