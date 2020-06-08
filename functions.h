07007340e0ff1af6a2569f9e24c15261a1c089e9
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2020-06-07 21:58:45 -0400

    añadido tipo de dato Pelicula y TDA lista

--- functions.h
+++ functions.h
@@ -2,5 +2,19 @@
 #define FUNCTIONS_H
 
 #include "sortedMap.h"
+#include "list.h"
+
+typedef struct Pelicula{
+
+    char id; //segunda posicion del archivo csv
+    char * titulo; //6ta posicion del archivo csv
+    int rating; //decima posicion del archivo csv
+    int year; // posicion 12 del archivo csv
+    List * generos; // posicion 13 del archivo csv
+    int numVotos; // posicion 14 del archivo csv
+    List * directores; // posicion 16 del archivo csv
+
+}Pelicula;
+
 
 #endif // FUNCTIONS_H
